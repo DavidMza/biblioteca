94d1fddccbfa6f5a027561b147556ec8431f2c75
emma <emma@emma-Lenovo-G465>
2015-06-25 02:06:44 -0300

    Merge....

--- controladores/portalWeb/persistencia/ConexionDBPortal.php
+++ controladores/portalWeb/persistencia/ConexionDBPortal.php
@@ -1,5 +1,16 @@
 <?php
 
 class ConexionDBPortal {
+    private $_conexion = null;
+    private $_usuario = 'cliente';
+    private $_clave = '1234567';
     
+    public function __construct() {
+        $this->_conexion = new PDO("mysql:dbname=biblioteca;host=localhost", $this->_usuario, $this->_clave);
+        $this->_conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+    function get_conexion() {
+        return $this->_conexion;
+    }
 }
--- controladores/portalWeb/persistencia/DBSentencias.php
+++ controladores/portalWeb/persistencia/DBSentencias.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author David
- */
 interface DBSentencias {
-    //put your code here
+    const TRAER_CARACTERISTICAS = "SELECT * FROM caracteristicas";
 }
